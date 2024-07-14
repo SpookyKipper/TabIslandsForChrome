9c3a7c537383e34c3dc4d374c1872d9b6834bc64
Spooky Kipper <88220209+SpookyKipper@users.noreply.github.com>
2024-07-14 09:11:46 +0800

    Code Cleanup

--- background.js
+++ background.js
@@ -1,35 +1,33 @@
 // Tab Group Naming Function //
 const nameTabGroup = (groupId) => {
     chrome.storage.sync.get(
-        {auto_created_group_name: "Island"},
+        { auto_created_group_name: "Island" },
         (items) => {
             if (items.auto_created_group_name != "") {
-                chrome.tabGroups.update(groupId, {title: items.auto_created_group_name})
+                chrome.tabGroups.update(groupId, { title: items.auto_created_group_name })
             }
         }
-      );
+    );
 }
 // Group Tabs //
-    // Check if pending url is here //
+// Check if pending url is here //
 const groupTabs = (tab) => {
     console.log(tab);
-    if ((tab.pendingUrl !== "" && typeof(tab.pendingUrl) != "undefined")) {
-      //  console.log("success" + tab)
+    if ((tab.pendingUrl !== "" && typeof (tab.pendingUrl) != "undefined")) {
         groupTabsAction(tab);
     } else {
-      //  console.log("failed" + tab)
         setTimeout(() => {
             checkTab(tab);
         }, 10)
     }
 }
-    // re-get tab info //
+// re-get tab info //
 const checkTab = (tab) => {
     chrome.tabs.get(tab.id, (tab) => {
         groupTabs(tab);
     });
 }
-    // actually group the tabs //
+// actually group the tabs //
 const groupTabsAction = (tab) => {
     if (tab.openerTabId && !(tab.pendingUrl.includes("chrome://")) && !(tab.pendingUrl.includes("extension://")) && !(tab.pendingUrl.includes("ntp.msn")) && tab.groupId === -1) {
         chrome.tabs.get(tab.openerTabId, (openerTab) => {
@@ -40,34 +38,16 @@ const groupTabsAction = (tab) => {
                     nameTabGroup(groupId);
                 });
             }
-        })  
+        })
     }
 }
 
-// Detect New Tabs opened via Middle Click //
+// Detect New Tabs //
 setTimeout(() => {
     chrome.tabs.onCreated.addListener((tab) => {
-        console.log("new tab created");
         groupTabs(tab);
     });
 }, 10);
-
-// Detect New Tabs opened via Anchor Tag with _blank target //
-chrome.runtime.onMessage.addListener((message) => {
-    if (message.type === 'link-opened') {
-        const openedUrl = message.url;
-        chrome.tabs.query({}, (tabs) => {
-            for (const tab of tabs) {
-                console.log(tabs);
-                if ((tab.pendingUrl === openedUrl)) {
-                    groupTabs(tab);
-                    break;
-                }
-            }
-        });
-    }
-});
-
 
 // Disband Tab Islands with only 1 tab //
 chrome.storage.sync.get(
@@ -94,20 +74,19 @@ chrome.storage.sync.get(
 // New tab in group keyboard shortcut //
 chrome.commands.onCommand.addListener((command) => {
     if (command === "newTabInGroup") {
-        chrome.tabs.query({active: true}, (tabs) => {
+        chrome.tabs.query({ active: true }, (tabs) => {
             const tab = tabs[0];
             if (!tab.pinned) {
-                chrome.tabs.create({active: true, index: tab.index+1}, (newtab) => {
+                chrome.tabs.create({ active: true, index: tab.index + 1 }, (newtab) => {
                     if (tab.groupId >= 0) {
-                        chrome.tabs.group({tabIds: [newtab.id], groupId: tab.groupId})
+                        chrome.tabs.group({ tabIds: [newtab.id], groupId: tab.groupId })
                     } else {
-                        chrome.tabs.group({tabIds: [newtab.id, tab.id]}, (groupId) => {
+                        chrome.tabs.group({ tabIds: [newtab.id, tab.id] }, (groupId) => {
                             nameTabGroup(groupId);
                         })
                     }
                 })
             }
-
         })
     }
 });
